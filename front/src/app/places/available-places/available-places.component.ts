0d8bc358fe99c33fba8b6a344d0a498f80fae963
Lucas Racoci <racoci.0@gmail.com>
2025-03-23 16:48:34 -0300

    Fetching places on init

--- front/src/app/places/available-places/available-places.component.ts
+++ front/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import {HttpClient, HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +12,14 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  ngOnInit() {
+    this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe({
+      next: (event) => {
+        console.log(event);
+      }
+    })
+  }
 }
